fa77daf2b77fbd0f6bf25054b36b3d6d8c5c1113
sam <sam@samnovotny.com>
2020-02-23 00:26:56 +0000

    Drive the Automation hat-mini relay

--- Sources/swifttest/main.swift
+++ Sources/swifttest/main.swift
@@ -1,22 +1,57 @@
+import SwiftyGPIO
 import Foundation
 
 print("Hello, Swift world!")
 
-DispatchQueue.global(qos: .background).async {
-  var i = 0
-  while true {
-      i += 1
-      print("😜\(i)")
-      Thread.sleep(forTimeInterval: 1)
-  }
+let gpios = SwiftyGPIO.GPIOs(for:.RaspberryPiPlusZero)
+
+//
+// Setup Relay
+//
+if let Relay = gpios[.P16] {
+    print("Relay set!")
+    Relay.direction = .OUT
+    DispatchQueue.global(qos: .background).async {
+      while true {
+          Relay.value = (Relay.value + 1) % 2
+        Thread.sleep(forTimeInterval: 0.5)
+      }
+    }
 }
 
-DispatchQueue.global(qos: .background).async {
-  for i in 1...5 {
-      print("🤢\(i)")
-  }
-}
+////
+//// Setup Button 1
+////
+//if let Button1 = gpios[.P5] {
+//    print("Button 1 set!")
+//    Button1.onRaising {
+//        gpio in
+//        Buzzer?.value = 1
+//        repeat {
+//            Thread.sleep(forTimeInterval: 0.25)
+//        } while gpio.value == 1
+//        Buzzer?.value = 0
+//    }
+//}
+//
+////
+//// Setup Button 2
+////
+//if let Button2 = gpios[.P6] {
+//    print("Button 2 set!")
+//    Button2.onRaising {
+//        gpio in
+//        LED2?.value = 1
+//        repeat {
+//            Thread.sleep(forTimeInterval: 0.25)
+//        } while gpio.value == 1
+//        LED2?.value = 0
+//    }
+//}
 
-repeat {
+//
+// Boring loop forever
+//
+while true {
     Thread.sleep(forTimeInterval: 1)
-} while true
+}
